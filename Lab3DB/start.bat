64dd0ddee8a9243f95960d754df9729fc6cec94c
KozakoPolako <79322668+KozakoPolako@users.noreply.github.com>
2021-06-20 22:54:50 +0200

    Update start.bat

--- Lab3DB/start.bat
+++ Lab3DB/start.bat
@@ -1,4 +1,4 @@
-docker run -d -p 2717:27017 -v %cd%/Current:/data/db --name mymongocr mymongo:1.0.3
+docker run -d -p 2717:27017 -v %cd%/Database:/data/db --name mymongocr mymongo:1.0.3
 ::ping 127.0.0.1 > nul
 docker exec -it mymongocr bash
 docker rm -f mymongocr
