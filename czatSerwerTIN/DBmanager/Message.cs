2a685641db8b4ff16fb6064763d228ad0798cd22
Varins <marinsans001@gmail.com>
2021-06-30 14:32:00 +0200

    Dodanie obsługi pobierania konwersacji
    stworzenie klasy Group oraz Message, modyfikacja metody LoadMessagesByGroupName w MongoConnect oraz oprogramowanie metody getMessageByGroup w CzatHub

---
+++ czatSerwerTIN/DBmanager/Message.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace czatSerwerTIN.DBmanager
+{
+    public class Message
+    {
+
+        public string sender { get; set; }
+        public string timeSent { get; set; }
+        public string msg { get; set; }
+
+        public Message(string sender, string msg, long unixTime )
+        {
+            this.sender = sender;
+            this.msg = msg;
+            DateTime time = DateTimeOffset.FromUnixTimeSeconds(unixTime).LocalDateTime;
+            this.timeSent = $"{time.ToShortDateString()} {time.ToShortTimeString()}";
+        }
+
+        public Message(string sender, string msg)
+        {
+            this.sender = sender;
+            this.msg = msg;
+            this.timeSent = DateTimeOffset.Now.ToUnixTimeSeconds().ToString();
+        }
+    }
+}
